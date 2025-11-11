de941f84c3e248e7d7d9ab596d66a6e6539e655d
bonalee20 <bboonnaaa0226@gmail.com>
2025-11-11 16:34:58 +0900

    11.11 16:34

---
+++ src/main/java/com/app/webnest/mapper/CommentLikeMapper.java
@@ -0,0 +1,10 @@
+package com.app.webnest.mapper;
+
+import com.app.webnest.domain.vo.CommentLikeVO;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface CommentLikeMapper {
+
+//    CommentLikeVO selectByPostIdcount (Long count);
+}
